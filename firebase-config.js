f0ad53ebe011214e648b5692f4b617c504fd554b
chimukatinos9-hash <chimukatinos9@gmail.com>
2026-06-16 14:38:52 +0200

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAiQRfjZ-3JdGR4cQU6iZ0yh0x695YPBQA",
+  authDomain: "finance-future-6944e.firebaseapp.com",
+  projectId: "finance-future-6944e",
+  storageBucket: "finance-future-6944e.firebasestorage.app",
+  messagingSenderId: "266248175562",
+  appId: "1:266248175562:web:af2b64c8b48b1ac0cc5933"
+};
+
+export const app = initializeApp(firebaseConfig);

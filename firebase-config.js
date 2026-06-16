ae7c11d9b944c4e5de0f7bd693a4ede3e038f7d7
chimukatinos9-hash <chimukatinos9@gmail.com>
2026-06-16 19:59:02 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAiQRfjZ-3JdGR4cQU6iZ0yh0x695YPBQA",
   authDomain: "finance-future-6944e.firebaseapp.com",
@@ -9,4 +13,6 @@ const firebaseConfig = {
   appId: "1:266248175562:web:af2b64c8b48b1ac0cc5933"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
